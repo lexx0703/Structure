78d55d48d1032feb0db4895a8c020af1d5bff33a
lexx0703 <lexx0703@gmail.com>
2023-03-13 10:43:48 +0300

    Initial commit

---
+++ Structure/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using Structure.Models;
+using System.Windows.Markup;
+using System.Collections.ObjectModel;
+using System.Windows.Forms;
+using System.Threading;
+using System.Web;
+using System.IO;
+using System.Data;
+
+
+
+namespace Structure
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BindingList<Product> _ProductData;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _ProductData = new BindingList<Product>()
+            {
+                new Product(){Name = "Название", Amount = 1, Capacity = 1, Price = 1, Number = 123, Weight = 1}
+            };
+            Products.ItemsSource = _ProductData;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Products.CancelEdit();
+            Products.Items.Refresh();
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            int i;
+            int rowCount = Products.Items.Count;
+            StreamWriter swExtLogFile = new StreamWriter("C:\\log.txt", true);
+            DataTable dt = new DataTable();
+            //Adding data To DataTable
+            dt.Columns.Add("Номер");
+            dt.Columns.Add("Название");
+            dt.Columns.Add("Количество");
+            dt.Columns.Add("Вес");
+            dt.Columns.Add("Цена");
+            dt.Columns.Add("Грузоподъёмность");
+            dt.Columns.Add("Допустимое количество");
+            dt.Columns.Add("Максимальная цена");
+
+            dt.Rows.Add("Столбцы: Номер", "Название", "Количество", "Вес", "Цена", "Грузоподъёмность", "Допустимое Количество", "Максимальная цена");
+            for (i = 0; i < rowCount-1; i++)
+            {
+                dt.Rows.Add(_ProductData[i].Number.ToString(), _ProductData[i].Name.ToString(), _ProductData[i].Amount.ToString(), _ProductData[i].Weight.ToString(), _ProductData[i].Price.ToString(), _ProductData[i].Capacity.ToString(), _ProductData[i].AmountAllowed.ToString(), _ProductData[i].MaxPrice.ToString());
+            }
+            //dt.Rows.Add(1, "test");
+
+
+
+            swExtLogFile.Write(Environment.NewLine);
+            foreach (DataRow row in dt.Rows)
+            {
+                object[] array = row.ItemArray;
+                for (i = 0; i < array.Length - 1; i++)
+                {
+                    swExtLogFile.Write(array[i].ToString() + " ");
+                }
+                swExtLogFile.WriteLine(array[i].ToString());
+            }
+            swExtLogFile.Write("*****END OF DATA****" + DateTime.Now.ToString());
+            swExtLogFile.Flush();
+            swExtLogFile.Close();
+        }
+    }
+}
---
+++ Structure/Models/Product.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+
+namespace Structure.Models
+{
+    class Product
+    {
+        private int _Price, _Amount, _Number;
+        private float _Weight, _Capacity, _AmountAllowed, _MaxPrice;
+        private string _Name;
+
+        public float Capacity
+        {
+            get { return _Capacity; }
+            set { _Capacity = value; }
+        }
+
+        public int Price
+        {
+            get { return _Price; }
+            set { _Price = value; }
+        }
+
+        public float AmountAllowed
+        {
+            get
+            {
+                if ((_Capacity / _Weight) >= _Amount)
+                {
+                    return _Amount;
+                }
+                else if ((_Capacity <= 0) || (_Weight <= 0))
+                {
+                    return 0;
+                }
+                else
+                {
+                    return MathF.Round(_Capacity / _Weight);
+                }
+            }
+            set { _AmountAllowed = value; }
+        }
+
+        public float MaxPrice
+        {
+            get
+            {
+                if (((_Capacity / _Weight) > _Amount) && (_Capacity > 0) && (_Weight > 0) && (_Amount > 0))
+                {
+                    return MathF.Round(_Amount * _Price);
+                }
+                else if (((_Capacity / _Weight) <= _Amount) && (_Capacity > 0) && (_Weight > 0) && (_Amount > 0))
+                {
+                    return MathF.Round(_Capacity / _Weight) * _Price;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            set { _MaxPrice = value; }
+        }
+
+        public int Number
+        {
+            get { return _Number; }
+            set { _Number = value; }
+        }
+
+        public float Weight
+        {
+            get { return _Weight; }
+            set { _Weight = value; }
+        }
+
+        public string Name
+        {
+            get { return _Name; } 
+            set { _Name = value; }
+        }
+        public int Amount
+        {
+            get { return _Amount; }
+            set { _Amount = value; }
+        }
+    }
+}
